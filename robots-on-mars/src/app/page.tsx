b3173003c66841dd7ec2e179b304e5454b1900a7
williampk <william.khamooshi@gmail.com>
2024-09-22 16:42:55 +0100

    added the ui to allow any inputs and display the output using material ui

--- robots-on-mars/src/app/page.tsx
+++ robots-on-mars/src/app/page.tsx
@@ -1,10 +1,78 @@
-export default function Home() {
-  return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <div>Robots on Mars!</div>
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center"></footer>
-    </div>
+"use client";
+import {
+  Button,
+  TextField,
+  Typography,
+  List,
+  ListItem,
+  ListItemText,
+} from "@mui/material";
+import { useState, ChangeEvent } from "react";
+import Grid from "@mui/material/Grid";
+
+const Home: React.FC = () => {
+  const defaultInputs = [
+    "5 3",
+    "1 1 E",
+    "RFRFRFRF",
+    "3 2 N",
+    "FRRFLLFFRRFLL",
+    "0 3 W",
+    "LLFFFLFLFL",
+  ];
+  const INPUT_SEPARATOR = "\n";
+  const [inputValue, setInputValue] = useState<string>(
+    defaultInputs.join(INPUT_SEPARATOR)
   );
-}
+  const [results, setResults] = useState<string[]>([]);
+
+  const handleInputChange = (e: ChangeEvent<HTMLTextAreaElement>) => {
+    setInputValue(e.target.value);
+  };
+
+  const handleSimulate = () => {
+    const data = ["ok1", "ok2"];
+    setResults(data);
+  };
+
+  return (
+    <Grid container spacing={2} margin={2}>
+      <Grid item>
+        <Typography variant="h6" gutterBottom>
+          Inputs:
+        </Typography>
+      </Grid>
+      <Grid item>
+        <TextField
+          id="simulationInput"
+          multiline
+          rows={10}
+          fullWidth
+          value={inputValue}
+          onChange={handleInputChange}
+          variant="outlined"
+          sx={{ marginBottom: 2, backgroundColor: "lightGrey" }}
+        />
+      </Grid>
+      <Grid item>
+        <Button variant="contained" color="primary" onClick={handleSimulate}>
+          Simulate
+        </Button>
+      </Grid>
+      <Grid item xs={10}>
+        <Typography variant="h6" gutterBottom>
+          Outputs:
+        </Typography>
+        <List>
+          {results.map((item, index) => (
+            <ListItem key={index}>
+              <ListItemText primary={item} />
+            </ListItem>
+          ))}
+        </List>
+      </Grid>
+    </Grid>
+  );
+};
+
+export default Home;
